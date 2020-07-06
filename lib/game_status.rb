5c62621ba9f6033400f8c42d5d83130b83cae5d0
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-07-06 19:32:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,6 +16,5 @@ def won?(board)
     position_3 = board[win_index_3]
     
     position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-      return win_combination
     end
   end
